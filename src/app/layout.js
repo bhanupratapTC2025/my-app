c8d231061c3fad1c7862d5372239324905a9c7cb
bhanupratapTC2025 <bhanupratap@yes-bm.com>
2025-05-09 12:33:07 +0530

    footer ko apne branch se hata diya

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
 import UpperNavbar from "@/components/UpperNavbar";
 import Navbar from "@/components/Navbar";
 import Breadcrumb from "@/components/Breadcrumb";
@@ -32,7 +31,6 @@ export default function RootLayout({ children }) {
           <Breadcrumb />
           {children}
         </main>
-        <Footer />
       </body>
     </html>
   );
