5b487a5943593c618debd088b12b35ae725213c1
Codebsbs <bhedsonkar@gmail.com>
2025-05-13 16:17:38 +0530

    sollve all brand realated problem

---
+++ src/app/brands/ray-ban/page.js
@@ -0,0 +1,14 @@
+import RayBan from '@/components/brands/rayban/RayBan'
+
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <RayBan/>
+    </div>
+  )
+}
+
+export default page
