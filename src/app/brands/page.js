5fd0be13ccc01c4c93dd4da6ced51d64721373ae
Codebsbs <bhedsonkar@gmail.com>
2025-05-13 18:04:52 +0530

    work on store page and brand page

--- src/app/brands/page.js
+++ src/app/brands/page.js
@@ -1,9 +1,11 @@
+import Titan from '@/components/brands/titan/Titan'
 import React from 'react'
 
 export default function Brands() {
   return (
     <div>
           brands here...
+          <Titan/>
           
     </div>
   )
